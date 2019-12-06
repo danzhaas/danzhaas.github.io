813394a3fc599b29eeab4341ad08079a3d92b09e
Dan Haas <dan.z.haas@gmail.com>
2019-12-05 22:43:33 -0500

    fine-tuning homepage style specifically margins/borders; added etch-a-sketch

---
+++ etch-a-sketch/README.md
@@ -0,0 +1,4 @@
+Etch a Sketch Project from The Odin Project's Web Dev 101 course
+Started 10/16/19
+
+This exercise was intended to teach how to use DOM manipulation to make a fun interactive game.  
---
+++ etch-a-sketch/etch-a-sketch.js
@@ -0,0 +1,139 @@
+//                              ETCH A SKETCH
+
+
+//          Creating the interactive Grid
+
+const grid = document.getElementById('container');
+var divsPerSide = 16;
+let fillGrid = function (divsPerSide) {
+    for(j=0;j<divsPerSide;j++) {
+        let fillRowWithPixels = function () {            
+            for (i=0;i<divsPerSide;i++) {
+                let addDiv = function () {
+                    let newDiv = document.createElement('div');
+                    newDiv.className="pixel";
+                    grid.appendChild(newDiv);
+                }
+                addDiv();
+            }
+        }            
+        fillRowWithPixels(divsPerSide);
+        
+        let newRow = function () {
+            let newDiv = document.createElement('div');
+            newDiv.className="break";
+            grid.appendChild(newDiv);
+        }
+        newRow();
+    }
+}
+
+fillGrid(divsPerSide);
+
+
+//          Select all pixels
+
+var updatePixelArray = function() {
+    pixelArray = document.querySelectorAll('.pixel');    
+}
+
+//          Resize Divs to fit Grid
+
+const gridDimensions = 512;
+var divDimensions = gridDimensions/divsPerSide;
+updatePixelArray();
+var updateDivSize = function() {
+    divDimensions = gridDimensions/divsPerSide;
+    for (i=0;i<pixelArray.length;i++) {
+        pixelArray[i].style.height=divDimensions+"px";
+        pixelArray[i].style.width=divDimensions+"px";
+        };
+    }
+updateDivSize();
+
+
+//          Reset and Resize (R and R) Button
+
+var rAndRButton = document.querySelector('#reset-and-resize');
+rAndRButton.addEventListener('click', () => {
+    while (grid.hasChildNodes()) {
+        grid.removeChild(grid.firstChild);
+    }
+    divsPerSide = prompt("How many squares wide?", 16);
+    if (divsPerSide=="") {divsPerSide=16}
+    fillGrid(divsPerSide);
+    updatePixelArray();
+    updateDivSize();
+    updateHoverEffect();
+})
+
+
+//          Hover effect
+
+var updateHoverEffect = function() {
+    updatePixelArray();
+    for (i=0;i<pixelArray.length;i++) {
+        pixelArray[i].addEventListener("mouseover", (e) => {
+            
+            if (colorButtonState=="random") {
+                randomColor="rgb("+Math.floor(Math.random()*256)+", "
+                    +Math.floor(Math.random()*256)+", "
+                    +Math.floor(Math.random()*256)+")";
+                trailColor=randomColor;
+            } else {
+                trailColor="rgb(0,0,0)";
+            }
+            e.target.style.backgroundColor = trailColor;
+            
+            if (opacityButtonState==true) {
+                if (e.target.style.opacity=="") {
+                    e.target.style.opacity=0.1
+                } else {
+                e.target.style.opacity=(parseFloat(e.target.style.opacity)+0.1)
+                }
+             } else {
+                e.target.style.opacity=""
+             }
+        })        
+    }
+}
+updateHoverEffect();
+
+
+//          Trail Color Button
+
+let trailColor="rgb(0,0,0)";
+var randomColor;
+    randomColor="rgb("+Math.floor(Math.random()*256)+", "
+        +Math.floor(Math.random()*256)+", "
+        +Math.floor(Math.random()*256)+")";
+
+var colorButton = document.querySelector('#color');
+let colorButtonState="black";
+colorButton.addEventListener('click', () => {
+    if (colorButtonState=="black") {
+        trailColor="rgb(0,0,0)";
+        colorButtonState="random";
+        colorButton.p.innerText = "Black Color"
+    } else {
+        trailColor=randomColor;
+        colorButtonState="black";
+        colorButton.p.innerText = "Randomize Colors"
+    }
+    updateHoverEffect();
+})
+
+//          Trail Opacity Mode Button
+
+var opacityButton = document.querySelector('#opacity');
+let opacityButtonState=false;
+opacityButton.addEventListener('click', () => {
+    if (opacityButtonState==false) {
+        opacityButtonState=true;
+        opacityButton.innerText = "Opacity Mode On"
+    } else {
+        opacityButtonState=false;
+        opacityButton.innerText = "<p>Opacity Mode Off</p>"
+    }
+    updateHoverEffect();
+})
