d489a250eac5bf5a31e3b1c46aadc97876b6b31d
Dan Haas <dan.z.haas@gmail.com>
2020-01-05 04:37:24 -0500

    added sql sample, updated nav and overall aesthetic

---
+++ js/slick-initialize-slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.sql-slideshow').slick({
+        dots:true,
+        autoplay:true,
+        autoplaySpeed:10000,
+        accessibility:true,
+        centerMode:true,        
+    });
+});
