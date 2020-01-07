eaa9a6c8e3cf69b05dfbf429db5c6cf17132fd39
Dan Haas <dan.z.haas@gmail.com>
2020-01-07 01:19:22 -0500

    side nav menu added

---
+++ js/side-nav.js
@@ -0,0 +1,31 @@
+let ii=localStorage.getItem("ii");
+if (ii!=="1") {
+    ii=0;
+} 
+if (ii=="1") {
+    closeNav();
+}
+    
+function openCloseToggle() {
+    if (ii==0) {
+        closeNav();        
+    } else {
+        openNav();        
+    };
+    if (typeof(Storage) !== "undefined") {
+        localStorage.setItem("ii", ii);
+    }
+    console.log(localStorage.getItem("ii"));
+}
+
+function openNav() {
+    document.getElementById
+    ("mySidenav").style.left = "0";
+    ii=0;
+}
+
+function closeNav() {
+    document.getElementById
+    ("mySidenav").style.left = "-15vw";
+    ii=1;
+}
