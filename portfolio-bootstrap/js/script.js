1633976e694e30b7d5a3dafdc7715aa6015119bc
Dan <dan.z.haas@gmail.com>
2020-07-13 13:54:09 -0400

    Live v1.03 implements responsive design to whole, design improvements to showcase

--- portfolio-bootstrap/js/script.js
+++ portfolio-bootstrap/js/script.js
@@ -1,13 +1,28 @@
-//carousel custom indicators
-const customCarsouselIndicators = $('.carousel-indicator');
-console.log(customCarsouselIndicators);
+// CAROUSEL INDICATOR TAG
+//purpose: add carousel custom indicators
+//activate appropriate indicators for viewport size depending on media breakpoint
+var smallViewportStatus = window.matchMedia("(max-width: 768px)")
+function tagIndicators(smallViewportStatus) {
+    if (smallViewportStatus.matches) {
+        //screen is xs or sm.  query the .ci-sm elements
+        customCarouselIndicators = $('.ci-xs-sm')
+    } else {
+        //screen is md or larger.  tag the #carousel-indicators group
+        customCarouselIndicators = $('.ci')
+    }
+}
+// console.log(customCarouselIndicators);
 $('#carouselControls').on('slide.bs.carousel', function (e) {
-    customCarsouselIndicators[e.from].classList.remove("active");
-    customCarsouselIndicators[e.to].classList.add("active");
+    customCarouselIndicators[e.from].classList.remove("active");
+    customCarouselIndicators[e.to].classList.add("active");
 })
+tagIndicators(smallViewportStatus);   // Call listener function at run time
+smallViewportStatus.addListener(tagIndicators) // Attach listener function on state changes
 
-//resume modal 
+
+//RESUME MODAL
 document.getElementById("resume-thumbnail").addEventListener("click", () => $('#resumeModal').modal('toggle'));
+
 
 //randomly-chosen z-adjectives
 // const zArray = ["zaftig", "zairean", "zairese", "zambian", "zany", "zapotec", "zealous", "zenithal", "zero", "zeroth", "zestful", "zesty", "zigzag", "zillion", "zimbabwean", "zionist", "zippy", "zodiacal", "zoftig", "zoic", "zolaesque", "zonal", "zonary", "zoological", "zoonotic", "zoophagous", "zoroastrian", "zygodactyl", "zygomatic", "zygomorphic", "zygomorphous", "zygotic", "zymoid", "zymolytic", "zymotic"];
